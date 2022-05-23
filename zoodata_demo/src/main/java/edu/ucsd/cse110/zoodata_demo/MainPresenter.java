c5de98769e8c95fdba42dc2d3256e69de4150ed6
Dylan Lukes <lukes.dylan@gmail.com>
2022-05-23 15:45:51 -0700

    presenter

---
+++ zoodata_demo/src/main/java/edu/ucsd/cse110/zoodata_demo/MainPresenter.java
@@ -0,0 +1,82 @@
+package edu.ucsd.cse110.zoodata_demo;
+
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.util.Pair;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import edu.ucsd.cse110.zoodata_demo.databinding.MainBinding;
+
+public class MainPresenter {
+    private final MainActivity activity;
+    private final MainViewModel model;
+    private final MainBinding view;
+
+    public MainPresenter(MainActivity activity, MainViewModel model, MainBinding view) {
+        this.activity = activity;
+        this.model = model;
+        this.view = view;
+
+        var adapter = new ExhibitListAdapter();
+
+        // Set up the recycler view.
+        var recyclerView = view.recyclerView;
+        recyclerView.setLayoutManager(new LinearLayoutManager(activity));
+        recyclerView.setAdapter(adapter);
+
+        // Set up the mocking button.
+        var mockButton = view.mockButton;
+        mockButton.setOnClickListener(this::onMockButtonClicked);
+
+        model.getExhibitsWithGroups().observe(activity, adapter::setExhibitsWithGroups);
+        model.getLastKnownCoords().observe(activity, adapter::setLastKnownCoords);
+    }
+
+    public void updateLastKnownCoords(double lat, double lng) {
+        activity.runOnUiThread(() -> {
+            model.setLastKnownCoords(Pair.create(lat, lng));
+        });
+    }
+
+    @SuppressLint("SetTextI18n")
+    private void onMockButtonClicked(View view) {
+        // TODO: could define this layout in an XML and inflate it, instead of defining in code...
+        var inputType = EditorInfo.TYPE_CLASS_NUMBER
+            | EditorInfo.TYPE_NUMBER_FLAG_SIGNED
+            | EditorInfo.TYPE_NUMBER_FLAG_DECIMAL;
+
+        final EditText latInput = new EditText(activity);
+        latInput.setInputType(inputType);
+        latInput.setHint("Latitude");
+        latInput.setText("32.746303");
+
+        final EditText lngInput = new EditText(activity);
+        lngInput.setInputType(inputType);
+        lngInput.setHint("Longitude");
+        lngInput.setText("-117.166595");
+
+        final LinearLayout layout = new LinearLayout(activity);
+        layout.setDividerPadding(8);
+        layout.setOrientation(LinearLayout.VERTICAL);
+        layout.addView(latInput);
+        layout.addView(lngInput);
+
+        var builder = new AlertDialog.Builder(activity)
+            .setTitle("Inject a Mock Location")
+            .setView(layout)
+            .setPositiveButton("Submit", (dialog, which) -> {
+                var lat = Double.parseDouble(latInput.getText().toString());
+                var lng = Double.parseDouble(lngInput.getText().toString());
+                updateLastKnownCoords(lat, lng);
+            })
+            .setNegativeButton("Cancel", (dialog, which) -> {
+                dialog.cancel();
+            });
+        builder.show();
+    }
+}
